49928e5e933dea049dfbeed1c2aed1286da8c5aa
amarghad <x.kroyo@outlook.com>
2024-10-05 01:40:06 +0100

    Create two different accounts at the application startup

--- src/main/java/ma/amarghad/accountservice/CompteServiceApplication.java
+++ src/main/java/ma/amarghad/accountservice/CompteServiceApplication.java
@@ -1,13 +1,40 @@
 package ma.amarghad.accountservice;
 
+import ma.amarghad.accountservice.entities.Account;
+import ma.amarghad.accountservice.enums.AccountType;
+import ma.amarghad.accountservice.repository.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class CompteServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CompteServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(AccountRepository accountRepository) {
+        return args -> {
+            Account currentAccount = Account.builder()
+                    .type(AccountType.CURRENT_ACCOUNT)
+                    .balance(1391.09)
+                    .createdAt(new Date())
+                    .build();
+            accountRepository.save(currentAccount);
+
+            Account savingAccount = Account.builder()
+                            .type(AccountType.SAVING_ACCOUNT)
+                            .balance(1493.00)
+                            .createdAt(new Date())
+                            .build();
+
+            accountRepository.save(savingAccount);
+        };
     }
 
 }
