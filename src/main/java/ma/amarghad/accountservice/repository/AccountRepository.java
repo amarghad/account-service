c6967a47638adae16a8fd70b7abdc3f5f78ecb25
amarghad <x.kroyo@outlook.com>
2024-10-05 17:22:07 +0100

    Use the AccountRepository as the expert projection for rest resource

--- src/main/java/ma/amarghad/accountservice/repository/AccountRepository.java
+++ src/main/java/ma/amarghad/accountservice/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package ma.amarghad.accountservice.repository;
 
 import ma.amarghad.accountservice.entities.Account;
+import ma.amarghad.accountservice.projections.AccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = AccountProjection.class)
 public interface AccountRepository extends JpaRepository<Account, String> {
 }
